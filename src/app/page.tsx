951500b6433cab13414a2c7137b5a906eae53156
Gabriel Egli <gabriel.j.egli+git@gmail.com>
2024-04-29 15:58:12 +0200

    fixed error and added oxlint & biomejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import Link from "next/link";
-
-import { CreatePost } from "~/app/_components/create-post";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 
